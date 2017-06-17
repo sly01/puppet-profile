3229378445341e8c1a87e875e635ca9f3a40dae3
root <root@aerkoc1.mylabserver.com>
2017-06-17 13:24:49 +0000

    initial commit

---
+++ manifests/base.pp
@@ -0,0 +1,16 @@
+class profile::base(
+	$ntp_servers = [
+		'0.ubuntu.pool.ntp.org',
+		'1.ubuntu.pool.ntp.org',
+	],
+) {
+	include ::ssh
+	class { '::ntp': 
+		servers => $ntp_servers,
+	}
+
+	if $facts['os']['family'] == 'RedHat' {
+		include ::profile::selinux
+	}
+
+}
