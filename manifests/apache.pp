000c1237c3c213bcc6fdb6b419f6c865b8440b22
sly01 <aerkoc01@gmail.com>
2017-06-18 13:02:20 +0000

    hiera data used

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,18 +1,24 @@
-class profile::apache {
+class profile::apache(
+	Hash $apache_vhost_servers,
+	Boolean $default_vhost = false,
+
+) {
 	class { '::apache':
-		default_vhost => true,
+		default_vhost => $default_vhost,
 	}
 	
-	$docroot = "/var/www/${facts['fqdn']}"
+	#$docroot = "/var/www/${facts['fqdn']}"
 
-	::apache::vhost { $facts['fqdn']:
-		port 		=> 80,
-		docroot => $docroot,
-		before  => File["${docroot}/index.html"],
-	}
+	#::apache::vhost { $facts['fqdn']:
+	#	port 		=> 80,
+	#	docroot => $docroot,
+	#	before  => File["${docroot}/index.html"],
+	#}
 
-	file { "${docroot}/index.html":
-		ensure => file,
-		content => "This is ${facts['fqdn']} web page\n",
-	}
+	#file { "${docroot}/index.html":
+	#	ensure => file,
+	#	content => "This is ${facts['fqdn']} web page\n",
+	#}
+
+	create_resources(::apache::vhost, $apache_vhost_servers)
 }
