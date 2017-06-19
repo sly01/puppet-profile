d39870ce12cab7763bb99967f0eaa91eba86c5a7
sly01 <aerkoc01@gmail.com>
2017-06-19 12:33:09 +0000

    Pam configuration added

--- manifests/base.pp
+++ manifests/base.pp
@@ -14,4 +14,11 @@ class profile::base(
 	}
 	class { '::rsyslog::client': }
 
+	#Pam module configuration
+	file_line { 'Replacing a line to /etc/pam.d/system-auth':
+		path => '/etc/pam.d/system-auth',
+	  line => 'password    requisite     pam_cracklib.so try_first_pass retry=3 minlen=8 ucredit=-1 ocredit=-1 dcredit=-1 lcredit=-1',
+		match => "^password    requisite.*$",
+	}
+
 }
